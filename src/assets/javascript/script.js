9b2230104764e29922986033be33833024997449
Emile Ramos <emilemramos@gmail.com>
2023-05-29 19:52:15 -0300

    Margem Automatica
    margem automatica permite redimencionar o navegador e a formatacao se adaptar

--- src/assets/javascript/script.js
+++ src/assets/javascript/script.js
@@ -15,3 +15,24 @@ document.addEventListener('DOMContentLoaded', function() {
       console.error('Elemento não encontrado!');
     }
   });
+ // JavaScript para manipulação do formulário
+ document.getElementById('registrationForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita o envio do formulário
+
+  // Obtém os valores dos campos
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  // Exibe os valores no console (você pode modificar essa parte para enviar os dados para um servidor)
+  console.log('Nome:', name);
+  console.log('E-mail:', email);
+  console.log('Senha:', password);
+
+  // Limpa os campos do formulário
+  document.getElementById('name').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('password').value = '';
+});
+
+
