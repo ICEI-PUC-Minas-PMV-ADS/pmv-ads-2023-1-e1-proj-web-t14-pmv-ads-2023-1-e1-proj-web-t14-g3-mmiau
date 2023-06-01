c063fa89af17ef0185ce29941697f879fe050ef4
Marcio Filho <marciufilho94@gmail.com>
2023-05-31 21:19:14 -0300

    Update script.js

--- src/assets/javascript/script.js
+++ src/assets/javascript/script.js
@@ -1,19 +1,22 @@
-openMenu.addEventlistener('click', () => {
+let menu = document.querySelector('#menu')
+let openMenu = document.querySelector('#openMenu')
+let closeMenu = document.querySelector('#openMenu')
+let anunciar = document.querySelector('#anunciar')
+
+
+openMenu.addEventListener('click', ()=>{
     menu.style.display = 'flex'
-
     menu.style.right = (menu.offsetWidth * -1) + 'px'
-
     openMenu.style.display = 'none'
 
     setTimeout(() => {
-        menu.style.opacity = '1'
-        
+        menu.style.opacity = '1'        
         menu.style.right = '0'
     }, 10)
 
 })
 
-closeMenu.addEventlistener('click', () => {
+closeMenu.addEventListener('click', () => {
     menu.style.opacity = '0'
 
     menu.style.right = (menu.offsetWidth * -1) + 'px'
@@ -23,4 +26,12 @@ closeMenu.addEventlistener('click', () => {
         openMenu.removeAttribute('style')
     }, 200)
 
+})
+
+// function cadastrargato(){
+//   alert("Botão 'Anunciar' clicado")
+//}
+
+anunciar.addEventListener('click', ()=>{
+    alert("Botão 'Anunciar' clicado")
 })
