a8e59724cc59a733769e84cffb349d7043ee8e5c
Emile Ramos <emilemramos@gmail.com>
2023-06-06 12:13:49 -0300

    Formularios

--- src/assets/javascript/script.js
+++ src/assets/javascript/script.js
@@ -15,29 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
       console.error('Elemento não encontrado!');
     }
   });
-
-
- // Script para manipulação do formulário
- document.getElementById('registrationForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita o envio do formulário
-
-  // Obtém os valores dos campos
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
-
-  // Exibe os valores no console (você pode modificar essa parte para enviar os dados para um servidor)
-  console.log('Nome:', name);
-  console.log('E-mail:', email);
-  console.log('Senha:', password);
-
-  // Limpa os campos do formulário
-  document.getElementById('name').value = '';
-  document.getElementById('email').value = '';
-  document.getElementById('password').value = '';
-});
-
-
+  
 // Função para verificar se o localStorage é suportado pelo navegador
 function isLocalStorageSupported() {
   try {
@@ -70,22 +48,50 @@ function cadastrarUsuario(name, email, password) {
   return false;
 }
 
-// Função para realizar o login do usuário
-function fazerLogin(email, password) {
-  // Verifica se o localStorage é suportado
-  if (isLocalStorageSupported()) {
-    // Obtém o objeto do usuário cadastrado
-    var user = JSON.parse(localStorage.getItem('user'));
-
-    // Verifica se o usuário existe e se a senha está correta
-    if (user && user.email === email && user.password === password) {
-      alert('Login realizado com sucesso!');
-      return true;
-    }
-  }
-
-  alert('E-mail ou senha inválidos!');}
-
+  document.addEventListener('DOMContentLoaded', function() {
+    // Evento de submit do formulário
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+      event.preventDefault();
   
-
-
+      // Obtém os valores dos campos de e-mail e senha
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
+  
+      // Realiza o login do usuário
+      fazerLogin(email, password);
+  
+      // Limpa os campos do formulário
+      document.getElementById('email').value = '';
+      document.getElementById('password').value = '';
+    });
+  
+    // Função para verificar se o localStorage é suportado pelo navegador
+    function isLocalStorageSupported() {
+      try {
+        var testKey = 'test';
+        localStorage.setItem(testKey, testKey);
+        localStorage.removeItem(testKey);
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+  
+    // Função para realizar o login do usuário
+    function fazerLogin(email, password) {
+      // Verifica se o localStorage é suportado
+      if (isLocalStorageSupported()) {
+        // Obtém o objeto do usuário cadastrado
+        var user = JSON.parse(localStorage.getItem('user'));
+  
+        // Verifica se o usuário existe e se a senha está correta
+        if (user && user.email === email && user.password === password) {
+          alert('Login realizado com sucesso!');
+          return true;
+        }
+      }
+  
+      alert('E-mail ou senha inválidos!');
+    }
+  });
+  
