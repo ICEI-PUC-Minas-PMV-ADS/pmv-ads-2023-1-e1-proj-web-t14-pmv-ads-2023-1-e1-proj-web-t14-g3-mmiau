c53960c49cee0ce16a6fe75c2d5ba1dfcf4b725d
Emile Ramos <emilemramos@gmail.com>
2023-06-06 11:45:46 -0300

    Login Signin
    testes das paginas de signin e login

--- src/assets/javascript/script.js
+++ src/assets/javascript/script.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
+
  // Script para manipulação do formulário
  document.getElementById('registrationForm').addEventListener('submit', function(event) {
   event.preventDefault(); // Evita o envio do formulário
@@ -35,3 +36,56 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('email').value = '';
   document.getElementById('password').value = '';
 });
+
+
+// Função para verificar se o localStorage é suportado pelo navegador
+function isLocalStorageSupported() {
+  try {
+    var testKey = 'test';
+    localStorage.setItem(testKey, testKey);
+    localStorage.removeItem(testKey);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+// Função para cadastrar um novo usuário
+function cadastrarUsuario(name, email, password) {
+  // Verifica se o localStorage é suportado
+  if (isLocalStorageSupported()) {
+    // Cria um objeto com as informações do usuário
+    var user = {
+      name: name,
+      email: email,
+      password: password
+    };
+
+    // Salva o objeto no localStorage
+    localStorage.setItem('user', JSON.stringify(user));
+
+    return true;
+  }
+
+  return false;
+}
+
+// Função para realizar o login do usuário
+function fazerLogin(email, password) {
+  // Verifica se o localStorage é suportado
+  if (isLocalStorageSupported()) {
+    // Obtém o objeto do usuário cadastrado
+    var user = JSON.parse(localStorage.getItem('user'));
+
+    // Verifica se o usuário existe e se a senha está correta
+    if (user && user.email === email && user.password === password) {
+      alert('Login realizado com sucesso!');
+      return true;
+    }
+  }
+
+  alert('E-mail ou senha inválidos!');}
+
+  
+
+
