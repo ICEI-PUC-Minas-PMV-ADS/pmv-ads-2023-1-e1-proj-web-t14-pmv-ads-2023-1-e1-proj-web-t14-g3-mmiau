1dd55bf826498b5f0ed3f09317d67951456e6ec3
Emile Ramos <emilemramos@gmail.com>
2023-06-25 20:59:36 -0300

    merge-codes

--- src/assets/javascript/anunciar.js
+++ src/assets/javascript/anunciar.js
@@ -1,45 +1,44 @@
-document.getElementById('cadastro-gato').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o envio do formulário
+document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('cadastro-gato-form');
+
+    form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Evitar o envio do formulário por padrão
   
-    // Obtém os valores dos campos do formulário
-    var nome = document.getElementById('nome-gato').value;
-    var sexo = document.getElementById('sexo-gato').value;
-    var idade = document.getElementById('idade-gato').value;
-    var raca = document.getElementById('raca-gato').value;
-    var castracao = document.getElementById('castracao-gato').value;
-    var vacinacao = document.getElementById('vacinacao-gato').value;
-    var foto = document.getElementById('foto-gato').value;
-  
-    // Cria um objeto com os dados do gato
-    var gato = {
-      nome: nome,
-      sexo: sexo,
-      idade: idade,
-      raca: raca,
-      castracao: castracao,
-      vacinacao: vacinacao,
-      foto: foto
-    };
-  
-    // Verifica se já existem gatos cadastrados no LocalStorage
-    var gatosCadastrados = localStorage.getItem('gatos');
-    if (gatosCadastrados) {
-      // Se já existem gatos cadastrados, converte a string JSON em um array
-      gatosCadastrados = JSON.parse(gatosCadastrados);
-    } else {
-      // Se não existem gatos cadastrados, cria um novo array vazio
-      gatosCadastrados = [];
-    }
-  
-    // Adiciona o novo gato ao array
-    gatosCadastrados.push(gato);
-  
-    // Converte o array em uma string JSON e armazena no LocalStorage
-    localStorage.setItem('gatos', JSON.stringify(gatosCadastrados));
-  
-    // Limpa os campos do formulário
-    document.getElementById('cadastro-gato').reset();
-  
-    // Exibe uma mensagem de sucesso ou redireciona para outra página, se necessário
+      var nome = document.getElementById('nome-gato').value;
+      var sexo = document.getElementById('sexo-gato').value;
+      var idade = document.getElementById('idade-gato').value;
+      var raca = document.getElementById('raca-gato').value;
+      var castracao = document.getElementById('castracao-gato').value;
+      var vacinacao = document.getElementById('vacinacao-gato').value;
+      var foto = document.getElementById('foto-gato').value;
+
+      // Criar um objeto com os dados do gato
+      var gato = {
+        nome: nome,
+        sexo: sexo,
+        idade: idade,
+        raca: raca,
+        castracao: castracao ? 'Castrado' : 'Não castrado',
+        vacinacao: vacinacao ? 'Vacinado' : 'Não vacinado',
+        foto: foto
+      };
+
+      // Obter os gatos cadastrados do localStorage
+      var gatosCadastrados = JSON.parse(localStorage.getItem('gatos')) || [];
+
+      // Adicionar o gato cadastrado à lista
+      gatosCadastrados.push(gato);
+
+      // Armazenar a lista atualizada no localStorage
+      localStorage.setItem('gatos', JSON.stringify(gatosCadastrados));
+      
+      // Exibir a mensagem de sucesso em um alerta
+      alert('Gato cadastrado com sucesso!');
+          
+      // Limpar os campos do formulário
+      form.reset();
+
+    });
   });
-  
+});
--- src/assets/javascript/script.js
+++ src/assets/javascript/script.js
@@ -1,24 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-    // ABRIR E FECHAR O MENU
-    openMenu.addEventListener('click', () => {
-      menu.style.display = 'flex';
-      menu.style.right = (menu.clientWidth * -1) + 'px';
-      openMenu.style.display = 'none';
+  // ABRIR E FECHAR O MENU
   
-      setTimeout(() => {
-        menu.style.opacity = '1';
-        menu.style.right = '0';
-      }, 10);
-    });
-  
-    closeMenu.addEventListener('click', () => {
-      menu.style.opacity = '0';
-      menu.style.right = (menu.clientWidth * -1) + 'px';
-  
-      setTimeout(() => {
-        menu.removeAttribute('style');
-        openMenu.removeAttribute('style');
-      }, 80);
-    });
+  openMenu.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    menu.style.right = (menu.clientWidth * -1) + 'px';
+    openMenu.style.display = 'none';
+
+    setTimeout(() => {
+      menu.style.opacity = '1';
+      menu.style.right = '0';
+    }, 10);
+  });
+
+  closeMenu.addEventListener('click', () => {
+    menu.style.opacity = '0';
+    menu.style.right = (menu.clientWidth * -1) + 'px';
+
+    setTimeout(() => {
+      menu.removeAttribute('style');
+      openMenu.removeAttribute('style');
+    }, 80);
+  });
 });   
